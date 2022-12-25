ea13cf78604293dd4e26322afa630a302f449266
Aaron <110748676+SurfWeb3@users.noreply.github.com>
2022-12-25 02:12:42 -0800

    Update 2_dex_migration.js

--- Migrations Folder/2_dex_migration.js
+++ Migrations Folder/2_dex_migration.js
@@ -1,5 +1,10 @@
 const Dex = artifacts.require("Dex");
 
+//artifact.require is used to import the contract
+
+//Migrations is the Smart Contract
+
 module.exports = function(deployer) {
+  //deployer is a Tuffle object used to deploy the contract which we imported
   deployer.deploy(Dex);
 };
