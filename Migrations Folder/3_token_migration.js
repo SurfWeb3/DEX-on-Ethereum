c44d701e1d441db0ee8b99f289af4b6488d47e4a
Aaron <110748676+SurfWeb3@users.noreply.github.com>
2022-12-25 02:15:38 -0800

    Update 3_token_migration.js

--- Migrations Folder/3_token_migration.js
+++ Migrations Folder/3_token_migration.js
@@ -1,6 +1,28 @@
 const Link = artifacts.require("Link");
 const Dex = artifacts.require("Dex");
 
+//artifact.require is used to import the contract
+
+//Migrations is the Smart Contract
+
 module.exports = async function(deployer, network, accounts) {
+  //deployer is a Tuffle object used to deploy the contract which we imported
+//"LINK" MEANS THE CRYPTO LINK 
   await deployer.deploy(Link);
+  //THE INSTRUCTIONS BELOW WERE STOPPED, BECAUSE IT IS BETTER TO DO THESE ACTIONS 
+//IN THE TESTING SOFTWARE, AND USE THIS FILE'S CODE TO JUST MIGRATE THE TOKEN
+//   let dex = await Dex.deployed()
+//   let link = await Link.deployed()
+//
+//         //we want to approve wallet:
+      //in the lesson link.approve is brfore wallet2.addToken
+//   await link.approve(dex.address, 500)
+   //we add token:
+//   await dex.addToken(web3.utils.fromUtf8("LINK"), link.address)
+
+    //and we want to deposit:
+//   await dex.deposit(100, web3.utils.fromUtf8("LINK"))
+//   let balanceOfLink = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+//   console.log(balanceOfLink);
+  
 };
