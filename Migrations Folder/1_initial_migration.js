424473fd6ede8ae45db9ff0d25978dee78ff88ce
Aaron <110748676+SurfWeb3@users.noreply.github.com>
2022-12-25 02:07:07 -0800

    Update 1_initial_migration.js

--- Migrations Folder/1_initial_migration.js
+++ Migrations Folder/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
 
+//artifact.require is used to import the contract
+
+//Migrations is the Smart Contract
+
 module.exports = function(deployer) {
+  //deployer is a Tuffle object used to deploy the contract which we imported
   deployer.deploy(Migrations);
 };
